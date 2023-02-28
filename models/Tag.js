d048e75cf670c4ed18d65802fdbf0efeee24f53b
cherry-aisha <113786104+cherry-aisha@users.noreply.github.com>
2023-02-28 21:40:37 +0000

    columns defines

--- models/Tag.js
+++ models/Tag.js
@@ -7,7 +7,27 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
-  },
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      product_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Product',
+          key: 'id',
+        }
+      },
+      tag_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Tag',
+          key: 'id',
+        }
+      },
+    },
   {
     sequelize,
     timestamps: false,
