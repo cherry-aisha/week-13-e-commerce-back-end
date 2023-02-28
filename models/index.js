53f49e4078d40875deb8aa996e82acaff43630cf
cherry-aisha <113786104+cherry-aisha@users.noreply.github.com>
2023-02-28 21:53:44 +0000

    updated index.js

--- models/index.js
+++ models/index.js
@@ -5,10 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsto(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+  })
 // Categories have many Products
+Category.hasMany(Product, {
+    foreignKey: "category_id",
+})
 
 // Products belongToMany Tags (through ProductTag)
+Category.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'Tags_For_Products'
+
+  });
 
 // Tags belongToMany Products (through ProductTag)
 
